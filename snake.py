38f67bade38cad86fe620187df6a2f2d86350584
Keira Hopkins <77685417+NotKeira@users.noreply.github.com>
2024-06-27 08:57:55 +0100

    Cleaned up a tiny bit

--- snake.py
+++ snake.py
@@ -8,14 +8,12 @@ delay = 0.1
 score = 0
 high_score = 0
 
-# Set up the screen
 wn = turtle.Screen()
 wn.title("Snake Game")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0)  # Turns off the screen updates
+wn.tracer(0)
 
-# Snake head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -24,7 +22,6 @@ head.penup()
 head.goto(0, 0)
 head.direction = "stop"
 
-# Snake food
 food = turtle.Turtle()
 food.speed(0)
 food.shape("circle")
@@ -34,7 +31,7 @@ food.goto(0, 100)
 
 segments = []
 
-# Pen
+
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -44,9 +41,6 @@ pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Score: 0  High Score: 0", align="center",
           font=("Courier", 24, "normal"))
-
-# Functions
-
 
 def go_up():
     if head.direction != "down":
